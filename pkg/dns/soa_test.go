ed76b08460f2778957b2b81bc1bf1223c0a3d49f
Louis Lefebvre <lefebl4@medtronic.com>
2023-06-05 15:29:54 -0500

    finish soa test

--- pkg/dns/soa_test.go
+++ pkg/dns/soa_test.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"gotest.tools/v3/assert"
@@ -12,9 +13,14 @@ type SoaExpected struct {
 	expected error
 }
 
+const (
+	ownerLoc = 0
+)
+
 func TestGetSoa(t *testing.T) {
 	testCases := []SoaExpected{
-		{[]string{"google.com"}, nil},
+		{[]string{"google.com."}, nil},
+		{[]string{"google.com.", "apple.com."}, nil},
 	}
 
 	ns, err := GetLocalNS()
@@ -24,8 +30,17 @@ func TestGetSoa(t *testing.T) {
 
 	for _, c := range testCases {
 		t.Run(fmt.Sprintf("Validating %s", c.input), func(t *testing.T) {
-			_, err := GetSoa(ns[0], c.input)
+			resp, err := GetSoa(ns[0], c.input)
 			assert.Equal(t, err, c.expected, "errors don't match")
+
+			for _, r := range resp {
+				ans := strings.FieldsFunc(r.Msg.Answer[0].String(), Split)
+				assert.Equal(t, ans[ownerLoc], r.Server, "dns message owner didn't match")
+			}
 		})
 	}
+}
+
+func Split(r rune) bool {
+	return r == '\t' || r == ' '
 }
