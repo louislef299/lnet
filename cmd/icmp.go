1a391731d5841822ca73fe07b8db42ca48a4373a
Louis Lefebvre <louislefebvre1999@gmail.com>
2023-09-08 16:30:47 -0500

    gathered icmp response

--- cmd/icmp.go
+++ cmd/icmp.go
@@ -4,10 +4,11 @@ Copyright © 2023 Louis Lefebvre <louislefebvre1999@gmail.com>
 package cmd
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os"
-	
+
 	"github.com/spf13/cobra"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
@@ -35,17 +36,30 @@ to quickly create a Cobra application.`,
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(addrs)
 
-		c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+		var srcIP string
+		for _, addr := range addrs {
+			src, _, _ := net.ParseCIDR(addr.String())
+			if src.To4() != nil {
+				//first ipv4 address on interface
+				srcIP = src.String()
+				break
+			}
+		}
+
+		c, err := icmp.ListenPacket("udp4", srcIP)
 		if err != nil {
 			log.Fatalf("listen err, %s", err)
 		}
 		defer c.Close()
 
 		wm := icmp.Message{
-			Type: ipv4.ICMPTypeEcho, Code: 0,
+			Type: ipv4.ICMPTypeEcho,
+			Code: 0,
 			Body: &icmp.Echo{
-				ID: os.Getpid() & 0xffff, Seq: 1,
+				ID:   os.Getpid() & 0xffff,
+				Seq:  1,
 				Data: []byte("HELLO-R-U-THERE"),
 			},
 		}
@@ -53,7 +67,7 @@ to quickly create a Cobra application.`,
 		if err != nil {
 			log.Fatal(err)
 		}
-		if _, err := c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetIP)}); err != nil {
+		if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP("192.168.4.1")}); err != nil {
 			log.Fatalf("WriteTo err, %s", err)
 		}
 
@@ -71,7 +85,40 @@ to quickly create a Cobra application.`,
 			log.Printf("got reflection from %v", peer)
 		default:
 			log.Printf("got %+v; want echo reply", rm)
+			os.Exit(1)
 		}
+
+		reply, ok := rm.Body.(*icmp.Echo)
+		if !ok {
+			switch b := rm.Body.(type) {
+			case *icmp.DstUnreach:
+				dest := ""
+				switch rm.Code {
+				case 0:
+					dest = "network"
+				case 1:
+					dest = "host"
+				case 2:
+					dest = "protocol"
+				case 3:
+					dest = "port"
+				case 4:
+					dest = "must-fragment"
+				default:
+					dest = "dest"
+				}
+				log.Fatalf("icmp %s-unreachable", dest)
+			case *icmp.PacketTooBig:
+				log.Fatalf("icmp packet-too-big (mtu %d)", b.MTU)
+			default:
+				log.Fatal("icmp non-echo response")
+			}
+		}
+		ip, err := icmp.ParseIPv4Header(reply.Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(ip)
 	},
 }
 
--- pkg/ip/ip.go
+++ pkg/ip/ip.go
@@ -1,6 +1,8 @@
 package ip
 
 import (
+	"encoding/binary"
+	"math"
 	"net"
 	"strings"
 )
@@ -17,4 +19,43 @@ func IsIPv6(address string) bool {
 		return false
 	}
 	return strings.Count(address, ":") >= 2
+}
+
+// Converts an IPv4 address into a uint32
+func ToUInt32(ip net.IP) uint32 {
+	return binary.BigEndian.Uint32([]byte(ip.To4()))
+}
+
+// Converts a uint32 into an IPv4 address
+func FromUInt32(u uint32) net.IP {
+	buff := make([]byte, 4)
+	binary.BigEndian.PutUint32(buff, u)
+	return net.IP(buff)
+}
+
+// DeltaIP returns the IPv4 delta-many places away
+func DeltaIP(ip net.IP, delta int) net.IP {
+	if delta == 0 {
+		return ip
+	}
+	i := ToUInt32(ip)
+	if delta < 0 {
+		i -= uint32(delta * -1)
+	} else if delta > 0 {
+		i += uint32(delta)
+	}
+	if i == math.MaxUint32 {
+		return ip //cant increment past broadcast
+	}
+	return FromUInt32(i)
+}
+
+// NextIP returns the next IPv4 in sequence
+func NextIP(ip net.IP) net.IP {
+	return DeltaIP(ip, 1)
+}
+
+// PrevIP returns the previous IPv4 in sequence
+func PrevIP(ip net.IP) net.IP {
+	return DeltaIP(ip, -1)
 }
