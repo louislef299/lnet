feb038ebbdcfb5237ff577f275defacf823a924e
Louis Lefebvre <louislefebvre1999@gmail.com>
2023-09-08 16:43:44 -0400

    wip

--- cmd/icmp.go
+++ cmd/icmp.go
@@ -5,17 +5,20 @@ package cmd
 
 import (
 	"log"
+	"net"
 	"os"
-	"strconv"
-
-	"github.com/louislef299/lnet/pkg/icmp"
+	
 	"github.com/spf13/cobra"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
+
+const targetIP = "8.8.8.8"
 
 // icmpCmd represents the icmp command
 var icmpCmd = &cobra.Command{
 	Use:   "icmp",
-	Short: "A brief description of your command",
+	Short: "Runs an ICMP scan on your local device",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -23,14 +26,51 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if usrID := os.Getuid(); usrID != 0 {
-			log.Println("current user id:", usrID)
-			log.Fatal("icmp command requires 'root' privileges(rerun with 'sudo')")
+		iface, err := net.InterfaceByName("wlp1s0")
+		if err != nil {
+			log.Fatal(err)
 		}
-		if i, _ := strconv.Atoi(os.Args[1]); i == 1 {
-			icmp.BottomOfIt()
-		} else {
-			icmp.IcmpScan()
+
+		addrs, err := iface.Addrs()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
+		if err != nil {
+			log.Fatalf("listen err, %s", err)
+		}
+		defer c.Close()
+
+		wm := icmp.Message{
+			Type: ipv4.ICMPTypeEcho, Code: 0,
+			Body: &icmp.Echo{
+				ID: os.Getpid() & 0xffff, Seq: 1,
+				Data: []byte("HELLO-R-U-THERE"),
+			},
+		}
+		wb, err := wm.Marshal(nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := c.WriteTo(wb, &net.IPAddr{IP: net.ParseIP(targetIP)}); err != nil {
+			log.Fatalf("WriteTo err, %s", err)
+		}
+
+		rb := make([]byte, 1500)
+		n, peer, err := c.ReadFrom(rb)
+		if err != nil {
+			log.Fatal(err)
+		}
+		rm, err := icmp.ParseMessage(ipv4.ICMPTypeEchoReply.Protocol(), rb[:n])
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch rm.Type {
+		case ipv4.ICMPTypeEchoReply:
+			log.Printf("got reflection from %v", peer)
+		default:
+			log.Printf("got %+v; want echo reply", rm)
 		}
 	},
 }
--- go.mod
+++ go.mod
@@ -9,9 +9,8 @@ require (
 	github.com/moby/moby v24.0.6+incompatible
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.16.0
-	github.com/vishvananda/netlink v1.1.0
-	golang.org/x/sync v0.3.0
-	golang.org/x/sys v0.12.0
+	golang.org/x/net v0.10.0
+	golang.org/x/sync v0.2.0
 	gotest.tools/v3 v3.4.0
 )
 
@@ -38,12 +37,11 @@ require (
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/vishvananda/netns v0.0.4 // indirect
-	golang.org/x/mod v0.12.0 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	golang.org/x/tools v0.13.0 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
