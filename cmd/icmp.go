459d10a83d833b93e6d2f039f0bc2f8ab0c899b4
Louis Lefebvre <louislefebvre1999@gmail.com>
2024-02-17 19:34:44 -0600

    chore: Remove long icmp empty desription

--- cmd/icmp.go
+++ cmd/icmp.go
@@ -16,12 +16,6 @@ import (
 var icmpCmd = &cobra.Command{
 	Use:   "icmp",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if usrID := os.Getuid(); usrID != 0 {
 			log.Println("current user id:", usrID)
